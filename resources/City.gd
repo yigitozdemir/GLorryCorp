2cf416e0cddd25485b4036856f552dc54196c245
Yigit Ozdemir1 <yozdemir@tr.ibm.com>
2023-07-14 20:29:36 +0300

    City name labels added over cities

---
+++ resources/City.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name City
+@export var name: String
+@export var position: Vector2
+@export var level: int
---
+++ scenes/main_scenes/game_scene/TileMap.gd
@@ -0,0 +1,30 @@
+extends TileMap
+
+@export var cities: Array[City] = []
+var tile_size: int = 8
+
+func _ready():
+	_create_city_name_labels()
+	pass
+	
+func _input(event):
+	if event.is_action_released("click"):
+		var pos = get_viewport().canvas_transform.affine_inverse() * (event.position)
+		var tile_pos = Vector2(int(pos.x / 8), int(pos.y / 8))
+		var clicked_city = search_city(tile_pos)
+		print(clicked_city)
+
+func search_city(tile_pos): ## search city in cities dictionary from cities list
+	for city in cities:
+		if city.position == tile_pos:
+			return city
+	return null
+
+func _create_city_name_labels() -> void:
+	for city in cities:
+		var label: Label = Label.new()
+		label.text = city.name
+		label.position = Vector2(city.position.x * tile_size - 12, city.position.y * tile_size - 20)
+		#label.add_theme_font_size_override("size", 22)
+		add_child(label)
+	pass
