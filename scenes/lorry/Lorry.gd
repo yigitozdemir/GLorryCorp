243e0b4bb30ef1d383c9a002dfb8f4840c219a1a
Yiğit Özdemir <yigitozdemir@yandex.com>
2023-07-15 11:12:36 +0300

    code refinement

--- scenes/lorry/Lorry.gd
+++ scenes/lorry/Lorry.gd
@@ -1,13 +1,23 @@
 extends Node2D
+class_name Lorry
+## nav_agent refers to the navigation agent a lorry contains
+@onready var nav_agent: NavigationAgent2D = get_node("nav_agent")
+## TileMap of the game
+@export var map: TileMap
 
-@onready var nav_agent: NavigationAgent2D = get_node("NavigationAgent2D")
+@export_category("Lorry properties")
+@export var brand: String
+@export var model: String
+@export var year: int
+@export var type: int
+@export var capacity: int
+
 var finish_pos = Vector2(558, 188)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	print(nav_agent.get_navigation_map())
-	nav_agent.set_navigation_map(((get_parent().get_node("TileMap"))as TileMap).get_navigation_map(0))
+	nav_agent.set_navigation_map(map.get_navigation_map(0))
 	print(nav_agent.get_navigation_map())
 	NavigationServer2D.map_force_update(nav_agent.get_navigation_map())
 	NavigationServer2D.map_set_active(nav_agent.get_navigation_map(), true)
@@ -22,7 +32,7 @@ func _ready():
 		print(c)
 	pass # Replace with function body.
 
-func _process(delta):
+func _process(_delta):
 	nav_agent.get_next_path_position()
 	#print(nav_agent.get_next_path_position())
 	#print("Target Reachable: " + str(nav_agent.is_target_reachable()))
--- scenes/main_scenes/game_scene/Camera2D.gd
+++ scenes/main_scenes/game_scene/Camera2D.gd
@@ -9,7 +9,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("camera_move_left"):
 		position.x -= camera_speed
 	if Input.is_action_pressed("camera_move_right"):
