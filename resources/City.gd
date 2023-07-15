b1e204e2278facb0f010ba309959857a12b2e3da
Yiğit Özdemir <yigitozdemir@yandex.com>
2023-07-15 11:23:04 +0300

    4 City resources

--- resources/City.gd
+++ resources/City.gd
@@ -3,3 +3,9 @@ class_name City
 @export var name: String
 @export var position: Vector2
 @export var level: int
+## Amount of coal resources (in tons)
+@export var res_coal: int
+## Amount of iron resources (in tons)
+@export var res_iron: int
+## Amount of workforce (not in tons)
+@export var res_workforce: int
