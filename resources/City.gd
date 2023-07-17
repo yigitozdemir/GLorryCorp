eab9ae51f3e14702737f67b972cbf61d606d999d
Yigit Ozdemir1 <yozdemir@tr.ibm.com>
2023-07-17 22:01:59 +0300

    18 Resource production speed in cities 19 Resource demand speed in cities 20 Add production to city warehouse periodically

--- resources/City.gd
+++ resources/City.gd
@@ -9,9 +9,24 @@ class_name City
 @export var res_iron: int
 ## Amount of workforce (not in tons)
 @export var res_workforce: int
+@export_category("Resource Demand")
 ## amount of coal demanded
 @export var demand_coal: int
 ## amount of iron demanded
 @export var demand_iron: int
 ## amount of workforce demanded
 @export var demand_workforce: int
+
+@export_category("Resource Production")
+@export var producting_coal: int
+@export var producting_iron: int
+@export var producting_workforce: int
+@export var production_period: float
+@export var since_last_production: float = 0
+
+@export_category("Resource demand")
+@export var demanding_coal: int
+@export var demanding_iron: int
+@export var demanding_workforce: int
+@export var demanding_period: float
+@export var since_last_demand: float = 0
--- scenes/main_scenes/game_scene/TileMap.gd
+++ scenes/main_scenes/game_scene/TileMap.gd
@@ -7,6 +7,23 @@ func _ready():
 	_create_city_name_labels()
 	pass
 	
+func _process(delta):
+	for city in cities:
+		city.since_last_production += delta
+		if city.since_last_production >= city.production_period:
+			city.res_coal += city.producting_coal
+			city.res_iron += city.producting_iron
+			city.res_workforce += city.producting_workforce
+			city.since_last_production = 0
+		
+		city.since_last_demand += delta
+		if city.since_last_demand >= city.demanding_period:
+			city.demand_iron += city.demanding_iron
+			city.demand_coal += city.demanding_coal
+			city.demand_workforce += city.demanding_workforce
+			city.since_last_demand = 0
+		pass
+	
 func _input(event):
 	if event.is_action_released("click"):
 		var pos = get_viewport().canvas_transform.affine_inverse() * (event.position)
